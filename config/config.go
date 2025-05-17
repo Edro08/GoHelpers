0fbc96bcdad244f5ff21501c14ad1684458bb4c0
Edro08 <Edrogarcia08@outlook.com>
2025-05-16 20:45:26 -0600

    Config - Delete param found

--- config/config.go
+++ config/config.go
@@ -5,14 +5,15 @@ import "fmt"
 // IConfig
 // ------------------------------------------------------------------------------------------------
 type IConfig interface {
-	GetString(keys string) (string, bool)
-	GetInt(keys string) (int, bool)
-	GetBool(keys string) (bool, bool)
-	GetMapInterface(keys string) (map[string]interface{}, bool)
-	GetMapString(keys string) (map[string]string, bool)
-	GetMapInt(keys string) (map[string]int, bool)
+	GetString(keys string) string
+	GetInt(keys string) int
+	GetBool(keys string) bool
+	GetMapInterface(keys string) map[string]interface{}
+	GetMapString(keys string) map[string]string
+	GetMapInt(keys string) map[string]int
 }
 
+// ------------------------------------------------------------------------------------------------
 // Implementation Methods
 // ------------------------------------------------------------------------------------------------
 const (
@@ -21,47 +22,47 @@ const (
 	mapInterfaceName = "mapInterface"
 )
 
-func (c *Config) GetString(keys string) (string, bool) {
-	value, found := c.getValue(keys)
-	return fmt.Sprintf("%v", value), found
+func (c *Config) GetString(keys string) string {
+	value, _ := c.getValue(keys)
+	return fmt.Sprintf("%v", value)
 }
 
-func (c *Config) GetInt(keys string) (int, bool) {
-	value, found := c.getValue(keys)
+func (c *Config) GetInt(keys string) int {
+	value, _ := c.getValue(keys)
 	if intValue, ok := value.(int); ok {
-		return intValue, found
+		return intValue
 	}
-	return 0, false
+	return 0
 }
 
-func (c *Config) GetBool(keys string) (bool, bool) {
-	value, found := c.getValue(keys)
+func (c *Config) GetBool(keys string) bool {
+	value, _ := c.getValue(keys)
 	if boolValue, ok := value.(bool); ok {
-		return boolValue, found
+		return boolValue
 	}
-	return false, false
+	return false
 }
 
-func (c *Config) GetMapInterface(keys string) (map[string]interface{}, bool) {
-	value, found := c.getMap(keys, mapInterfaceName)
+func (c *Config) GetMapInterface(keys string) map[string]interface{} {
+	value, _ := c.getMap(keys, mapInterfaceName)
 	if value, ok := value.(map[string]interface{}); ok {
-		return value, found
+		return value
 	}
-	return nil, false
+	return nil
 }
 
-func (c *Config) GetMapString(keys string) (map[string]string, bool) {
-	value, found := c.getMap(keys, mapStringName)
+func (c *Config) GetMapString(keys string) map[string]string {
+	value, _ := c.getMap(keys, mapStringName)
 	if value, ok := value.(map[string]string); ok {
-		return value, found
+		return value
 	}
-	return nil, false
+	return nil
 }
 
-func (c *Config) GetMapInt(keys string) (map[string]int, bool) {
-	value, found := c.getMap(keys, mapIntName)
+func (c *Config) GetMapInt(keys string) map[string]int {
+	value, _ := c.getMap(keys, mapIntName)
 	if value, ok := value.(map[string]int); ok {
-		return value, found
+		return value
 	}
-	return nil, false
+	return nil
 }
